bd1dd46f91667d96441d41cc911c22dcf9d4fdd1
avijain09 <a_jain2@ce.iitr.ac.in>
2024-04-13 17:08:46 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css"
+// import React, { useState, useEffect } from "react";
 import Header from "./components/common/header/Header"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import About from "./components/about/About"
@@ -9,7 +10,9 @@ import Blog from "./components/blog/Blog"
 import Contact from "./components/contact/Contact"
 import Footer from "./components/common/footer/Footer"
 import Home from "./components/home/Home"
+// import StatePage from "./components/state/statePage"
 function App() {
+  // const [selectedState, setSelectedState] = useState("Uttarakhand");
   return (
     <>
       <Router>
@@ -22,6 +25,10 @@ function App() {
           <Route exact path='/pricing' component={Pricing} />
           <Route exact path='/journal' component={Blog} />
           <Route exact path='/contact' component={Contact} />
+          {/* <Route
+                            path="/:selectedState"
+                            element={<StatePage selectedState={selectedState} />}
+                        /> */}
         </Switch>
         <Footer />
       </Router>
