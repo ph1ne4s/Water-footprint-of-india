8a77ba0d5685a698d274e353840259d28b5f8516
avijain09 <a_jain2@ce.iitr.ac.in>
2024-04-23 16:59:30 +0530

    contactus changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
           <div className="left row">
             <iframe
               title="Google Map"
-              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d904726.6131739549!2d85.24565535!3d27.65273865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1652535615693!5m2!1sen!2snp"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3459.999978746913!2d77.8924288151025!3d29.865191181972836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39090c3c4485ed5d%3A0x5aaac77cf5a9ff90!2sIndian%20Institute%20of%20Technology%20Roorkee!5e0!3m2!1sen!2sin!4v1652628451087!5m2!1sen!2sin"
               width="600"
               height="450"
               style={{ border: 0 }}
--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <div className='container'>
           <div className='row'>
             <Heading subtitle='' title='What is Water Footprint?' />
-            <p>The water footprint is the total amount of freshwater used directly and indirectly to produce goods and services. It includes rainwater, surface/groundwater used in agriculture, industry, and domestic settings, and water needed to dilute pollutants. This concept helps assess environmental impact, identify conservation opportunities, and promote sustainable water management. It aids companies, governments, and individuals in understanding dependencies and enhancing water security.</p>
+            <p className='larger-font'>The water footprint is the total amount of freshwater used directly and indirectly to produce goods and services. It includes rainwater, surface/groundwater used in agriculture, industry, and domestic settings, and water needed to dilute pollutants. This concept helps assess environmental impact, identify conservation opportunities, and promote sustainable water management. It aids companies, governments, and individuals in understanding dependencies and enhancing water security.</p>
             <div className='button'>
               
             </div>
